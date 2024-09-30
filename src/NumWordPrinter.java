8b10726e4632829eece0a79dd0724278a690e8f3
tahirdotz <tahirzaman075@gmail.com>
2024-09-30 22:10:29 +0600

    Task FInished all tests passed

--- src/NumWordPrinter.java
+++ src/NumWordPrinter.java
@@ -1,6 +1,22 @@
 public class NumWordPrinter {
-    public String printWord(int n,boolean toUppercase)
-    {
 
+    public String printWord(int n, boolean toUpperCase) {
+        String result = "";
+
+        if (n % 2 == 0 && n % 11 == 0 && n % 13 == 0) {
+            result = "EvenPrimeLucky";
+        } else if (n % 2 == 0 && n % 11 == 0) {
+            result = "EvenPrimeEleven";
+        } else if (n % 13 == 0) {
+            result = "LuckyThirteen";
+        } else if (n % 11 == 0) {
+            result = "PrimeEleven";
+        } else if (n % 2 == 0) {
+            result = "Even";
+        } else {
+            result = "OddOneOut";
+        }
+
+        return toUpperCase ? result.toUpperCase() : result;
     }
 }
